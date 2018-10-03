92a90b7a0009117c59aed478aa9af4f327fad7d6
Lucas Piazzi <lpiazzi26@gmail.com>
2018-10-03 11:44:07 -0300

    atualização da função de relação

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -115,19 +115,31 @@
 		})();
     </script>
 
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
     <script>
     //Codigo das relações
     function relation() {
-
+        // determina se duas classes estão conectadas
      mxGraphModel.prototype.getCell = function(	id	)
     {
+
         if(mxCell.prototype.isEdge)
         {
-        alert("Você conectou algo");
+            if(mxCell.prototype.getStyle)
+            {
+                if(mxCell.prototype.isVertex)
+                {
+
+                    alert("Você conectou algo");
+
+                }
+            }
+
         }
     }
 
