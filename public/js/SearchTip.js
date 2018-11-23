cb1e0a43a2eabf3ceb3327b9a3a1a4ddaea03a45
Lucas Piazzi <lpiazzi26@gmail.com>
2018-11-23 16:17:25 -0200

    Bugfix da SearchTip

--- public/js/SearchTip.js
+++ public/js/SearchTip.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#search-tip-input").on("keyup", function () {
         var value = $(this).val().toLowerCase();
-        $("#tipSearch *").filter(function(){
+        $(".collapsed-box").filter(function(){
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
