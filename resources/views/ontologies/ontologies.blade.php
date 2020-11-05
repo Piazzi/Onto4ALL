93872d132543201f3ff4b3757d176cc4e468a9c1
Lucas Piazzi <lpiazzi26@gmail.com>
2020-11-05 09:02:51 -0300

    Conserta bug em que a SearchBar do Ontology Manager não carrega

--- resources/views/ontologies/ontologies.blade.php
+++ resources/views/ontologies/ontologies.blade.php
@@ -268,7 +268,7 @@
         {{ $ontologies->links() }}
     </ul>
     <!-- Filter -->
-    <script type="text/javascript" src="js/SearchBar.js"></script>
+    <script type="text/javascript" src="{{asset('js/SearchBar.js')}}"></script>
 
 @stop
 
