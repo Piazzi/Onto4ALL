a4b41b4d746de49725a57b4831912e289e3fd6ec
Lucas Piazzi <lpiazzi26@gmail.com>
2020-11-16 12:00:34 -0300

    Adiciona atributo lang na tag HTML do editor

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html @if(app()->getLocale() == 'en')lang="en"@else  lang="pt"@endif>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
