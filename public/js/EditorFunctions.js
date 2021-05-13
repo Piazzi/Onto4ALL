0fcaba3bdfe4bb2864da736cfd8ab72cd49c9f29
Lucas Piazzi <40416044+Piazzi@users.noreply.github.com>
2021-05-13 16:51:22 -0300

    Adiciona search de tips na sidebar  direita

--- public/js/EditorFunctions.js
+++ public/js/EditorFunctions.js
@@ -57,5 +57,29 @@ $(document).ready(function () {
     });
 });
 
+   /**
+     * When a user clicks on the ontology palette the name of
+     * the class or relation is searched in the tips menu
+     */
+    $(".geSidebar .geItem").click(function () {
+        let name =  $(this).attr('class');
+        name = name.replace("geItem", "").trim();
+        if(name != 'Class' && name != 'Callout' && name != 'Textbox' && name != 'Text' && name != 'Instance' && name != 'new_relation')
+        {
+            $('#search-tip-input').attr('value', name);
+            $("#menu-scroll .collapsed-box").filter(function () {
+                $(this).toggle($(this).text().toLowerCase().indexOf(name) > -1)
+            });
+        }
+    });
+
+    $("#search-tip-input").on("keyup", function () {
+        let value = $(this).val().toLowerCase();
+
+        $("#menu-scroll .collapsed-box").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+
+    });
 
 
