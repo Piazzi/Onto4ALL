14bd7b910d9a1b9b2dc2c4927249bed60440ed76
Vinicius Corbelli <v.corbelli71@gmail.com>
2021-09-16 16:32:04 -0300

    Correções das views do thesaurus

--- tests/Feature/OntologiesTest.php
+++ tests/Feature/OntologiesTest.php
@@ -10,15 +10,6 @@ use Tests\TestCase;
 class OntologiesTest extends TestCase
 {
 
-    /*
-    Route::resource('/ontologies', 'OntologyController')->middleware('can:eModelador');
-    Route::get('/ontologies/download/{userId}/{ontologyId}', 'OntologyController@downloadXML')->name('ontologies.download')->middleware('can:eModelador');
-    Route::get('/ontologies/downloadOWL/{userId}/{ontologyId}', 'OntologyController@downloadOWL')->name('ontologies.downloadOWL')->middleware('can:eModelador');
-    Route::put('/ontologies/favourite/{userId}/{ontologyId}', 'OntologyController@saveAsFavourite')->name('ontologies.favourite')->middleware('can:eModelador');
-    Route::put('/ontologies/normal/{userId}/{ontologyId}', 'OntologyController@saveAsNormal')->name('ontologies.normal')->middleware('can:eModelador');
-    Route::post('/updateOrCreate', 'OntologyController@updateOrCreate');
-    */
-
     public function test_can_see_ontologies_pt()
     {
         $user = User::inRandomOrder()->first();
@@ -149,58 +140,6 @@ class OntologiesTest extends TestCase
 
         $response = $this->get('en/ontologies/' . $ontology->id . '/edit');
         $response->assertStatus(200);
-    }
-
-    public function test_can_delete_ontologies_pt()
-    {
-        $faker = \Faker\Factory::create();
-        $user = User::inRandomOrder()->first();
-        if ($user == null)
-            $user = factory(User::class)->create([
-                'password' => bcrypt($password = 'teste'),
-                'categorie' => 'administrador',
-            ]);
-        Auth::loginUsingId($user->id);
-
-        $ontology = Ontology::inRandomOrder()->first();
-        if ($ontology == null) {
-            $ontology = new Ontology();
-            $ontology->name = $faker->word;
-            $ontology->xml_string = $faker->word;
-            $ontology->user_id = $user->id;
-            $ontology->created_by = $user->name;
-            $ontology->favourite = 0;
-            $ontology->save();
-        }
-
-        $response = $this->delete('pt/ontologies/' . $ontology->id);
-        $response->assertStatus(302);
-    }
-
-    public function test_can_delete_ontologies_en()
-    {
-        $faker = \Faker\Factory::create();
-        $user = User::inRandomOrder()->first();
-        if ($user == null)
-            $user = factory(User::class)->create([
-                'password' => bcrypt($password = 'teste'),
-                'categorie' => 'administrador',
-            ]);
-        Auth::loginUsingId($user->id);
-
-        $ontology = Ontology::inRandomOrder()->first();
-        if ($ontology == null) {
-            $ontology = new Ontology();
-            $ontology->name = $faker->word;
-            $ontology->xml_string = $faker->word;
-            $ontology->user_id = $user->id;
-            $ontology->created_by = $user->name;
-            $ontology->favourite = 0;
-            $ontology->save();
-        }
-
-        $response = $this->delete('en/ontologies/' . $ontology->id);
-        $response->assertStatus(302);
     }
 
     public function test_can_update_ontologies_pt()
