fe235fb10d5c7355c86dd79b4b186afb7d4b7160
Lucas Piazzi <40416044+Piazzi@users.noreply.github.com>
2021-09-20 10:02:12 -0300

    Remove botão de inserir link

--- public/grapheditor/js/Toolbar.js
+++ public/grapheditor/js/Toolbar.js
@@ -168,9 +168,9 @@ Toolbar.prototype.init = function()
 		this.addDropDownArrow(this.edgeStyleMenu, 'geSprite-orthogonal', 44, 50, 0, 0, 22, -4);
 	}
 
-	this.addSeparator();
-	var insertMenu = this.addMenu('', mxResources.get('insert') + ' (' + mxResources.get('doubleClickTooltip') + ')', true, 'insert', null, true);
-	this.addDropDownArrow(insertMenu, 'geSprite-plus', 38, 48, -4, -3, 36, -8);
+	//this.addSeparator();
+	//var insertMenu = this.addMenu('', mxResources.get('insert') + ' (' + mxResources.get('doubleClickTooltip') + ')', true, 'insert', null, true);
+	//this.addDropDownArrow(insertMenu, 'geSprite-plus', 38, 48, -4, -3, 36, -8);
 	//this.addTableDropDown();
 };
 
