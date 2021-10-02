e2252f110c7b97c3ba83a2a51794e19fa1c0e28c
Vinicius Corbelli <v.corbelli71@gmail.com>
2021-10-02 18:50:33 -0300

    Mostrando informações da ontologia na tela

--- resources/views/ontologies/ontologies-show.blade.php
+++ resources/views/ontologies/ontologies-show.blade.php
@@ -136,6 +136,47 @@
                 <label>XML</label>
                 <textarea class="form-control" rows="13" cols="100" id="textarea">{{$ontology->xml_string}}</textarea>
             </div>
+
+            <div class="row">
+                <div class="informations">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <h4 id="#nome"></h4>
+                            <label>SubClassOf</label>
+                            <label id="#SubClassOf" class="form-control"></label>
+                            <label>DisjointWith</label>
+                            <label id="#DisjointWith" class="form-control"></label>
+                            <label>Equivalence</label>
+                            <label id="#Equivalence" class="form-control"></label>
+                            <label>TargetForKey</label>
+                            <label id="#TargetForKey" class="form-control"></label>
+                            <label>Instances</label>
+                            <label id="#Instances" class="form-control"></label>
+                            <label>label</label>
+                            <label id="#label" class="form-control"></label>
+                            <label>seeAlso</label>
+                            <label id="#seeAlso" class="form-control"></label>
+                            <label>isDefinedBy</label>
+                            <label id="#isDefinedBy" class="form-control"></label>
+                            <label>comment</label>
+                            <label id="#comment" class="form-control"></label>
+                            <label>versionInfo</label>
+                            <label id="#versionInfo" class="form-control"></label>
+                            <label>priorVersion</label>
+                            <label id="#priorVersion" class="form-control"></label>
+                            <label>deprecated</label>
+                            <label id="#isDefinedBy" class="form-control"></label>
+                            <label>incompatibleWith</label>
+                            <label id="#incompatibleWith" class="form-control"></label>
+                            <label>backwardCompatibleWith</label>
+                            <label id="#backwardCompatibleWith" class="form-control"></label>
+                            <hr>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
             <a href="{{route('ontologies.index', app()->getLocale())}}">
                 <button class="btn btn-success btn-block" type="button">Go back</button>
             </a>
@@ -144,19 +185,111 @@
     </div>
 @stop
 
+@php
+    $xml = str_replace('"','\"',$ontology->xml_string);
+@endphp
+
 @section('footer')
 
 @push('js')
 
+<script src="../../grapheditor/src/js/util/mxUtils.js"></script>
 <script type="text/javascript">
 
-    var doc = mxUtils.parseXml('<mxGraphModel dx="894" dy="654" grid="1" gridSize="10" guides="1" tooltips="1" connect="0" arrows="0" fold="1" page="1" pageScale="2" pageWidth="827" pageHeight="1169"><root><mxCell id="0"/><mxCell id="1" parent="0"/><object SubClassOf="Label" Constraint="" DisjointWith="" Equivalence="" TargetForKey="" Instances="" label="Thing" seeAlso="" isDefinedBy="" comment="" versionInfo="" priorVersion="" deprecated="" incompatibleWith="" backwardCompatibleWith="" id="2"><mxCell style="ellipse;whiteSpace=wrap;html=1;aspect=fixed;dashed=1;Class;" vertex="1" parent="1"><mxGeometry x="20" y="20" width="80" height="80" as="geometry"/></mxCell></object><object label="Label" SubClassOf="" Constraint="" DisjointWith="" Equivalence="" TargetForKey="" Instances="" seeAlso="" isDefinedBy="" comment="" versionInfo="" priorVersion="" deprecated="" incompatibleWith="" backwardCompatibleWith="" id="3"><mxCell style="ellipse;whiteSpace=wrap;html=1;aspect=fixed;strokeColor=#f39c12;Class;" vertex="1" parent="1"><mxGeometry x="250" y="90" width="80" height="80" as="geometry"/></mxCell></object><object label="Label" SubClassOf="Label" Constraint="" DisjointWith="" Equivalence="" TargetForKey="" Instances="" seeAlso="" isDefinedBy="" comment="" versionInfo="" priorVersion="" deprecated="" incompatibleWith="" backwardCompatibleWith="" id="4"><mxCell style="ellipse;whiteSpace=wrap;html=1;aspect=fixed;strokeColor=#f39c12;Class;" vertex="1" parent="1"><mxGeometry x="240" y="290" width="80" height="80" as="geometry"/></mxCell></object><object label="is_a" domain="Label" range="Label" inverseOf="" equivalentTo="" subpropertyOf="" disjointWith="" functional="" inverseFunctional="" transitive="" symetric="" asymmetric="" reflexive="" irreflexive="" seeAlso="" isDefinedBy="" comment="" versionInfo="" priorVersion="" deprecated="" incompatibleWith="" backwardCompatibleWith="" DisjointWith="" Equivalence="" id="5"><mxCell style="html=1;verticalAlign=bottom;endArrow=block;strokeColor=#004C99;Relation;" edge="1" parent="1" source="4" target="3"><mxGeometry width="80" relative="1" as="geometry"><mxPoint x="20" y="390" as="sourcePoint"/><mxPoint x="100" y="390" as="targetPoint"/><Array as="points"><mxPoint x="200" y="230"/></Array></mxGeometry></mxCell></object><object label="is_a" domain="Thing" range="Label" inverseOf="" equivalentTo="" subpropertyOf="" disjointWith="" functional="" inverseFunctional="" transitive="" symetric="" asymmetric="" reflexive="" irreflexive="" seeAlso="" isDefinedBy="" comment="" versionInfo="" priorVersion="" deprecated="" incompatibleWith="" backwardCompatibleWith="" id="6"><mxCell style="html=1;verticalAlign=bottom;endArrow=block;strokeColor=#004C99;Relation;" edge="1" parent="1" source="2" target="3"><mxGeometry width="80" relative="1" as="geometry"><mxPoint x="20" y="390" as="sourcePoint"/><mxPoint x="100" y="390" as="targetPoint"/></mxGeometry></mxCell></object></root></mxGraphModel>');
+   /*var doc = mxUtils.parseXml("<?php echo"$xml"?>");
     var graph = new Graph(container, null, null, null, null);
     var outputSvg = graph.getSvg("#FFFFFF", 1, null, null, true, null, null);
 
-    document.getElementById('graph').appendChild(outputSvg);
+    document.getElementById('graph').appendChild(outputSvg);*/
+
+    let parser, xmlDoc;
+    parser = new DOMParser();
+    
+    xmlDoc = parser.parseFromString("<?php echo"$xml"?>", "text/xml");
+
+
+    for (let i = 0; i < xmlDoc.getElementsByTagName("object").length; i++){
+        if(xmlDoc.getElementsByTagName("object")[i].getAttribute("label") != null ){
+
+            if (i != 0) {
+                var newel = $('.informations:last').clone();
+                $(newel).insertAfter(".informations:last");
+            }
+
+            var SubClassOf;
+            var DisjointWith;
+            var Equivalence;
+            var TargetForKey;
+            var Instances;
+            var label;
+            var seeAlso;
+            var isDefinedBy;
+            var comment;
+            var versionInfo;
+            var priorVersion;
+            var deprecated;
+            var incompatibleWith;
+            var backwardCompatibleWith;
+            
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("SubClassOf") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("SubClassOf") != "")
+                SubClassOf = xmlDoc.getElementsByTagName("object")[i].getAttribute("SubClassOf");
+            
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("DisjointWith") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("DisjointWith") != "")
+                DisjointWith = xmlDoc.getElementsByTagName("object")[i].getAttribute("DisjointWith");
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("Equivalence") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("Equivalence") != "")
+                Equivalence = xmlDoc.getElementsByTagName("object")[i].getAttribute("Equivalence");
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("Instances") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("Instances") != "")
+                Instances = xmlDoc.getElementsByTagName("object")[i].getAttribute("Instances");
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("label") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("label") != "")
+                label = xmlDoc.getElementsByTagName("object")[i].getAttribute("label");
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("seeAlso") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("seeAlso") != "")
+                seeAlso = xmlDoc.getElementsByTagName("object")[i].getAttribute("seeAlso");
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("isDefinedBy") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("isDefinedBy") != "")
+                isDefinedBy = xmlDoc.getElementsByTagName("object")[i].getAttribute("isDefinedBy");
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("comment") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("comment") != "")
+                comment = xmlDoc.getElementsByTagName("object")[i].getAttribute("comment");
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("versionInfo") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("versionInfo") != "")
+                versionInfo = xmlDoc.getElementsByTagName("object")[i].getAttribute("versionInfo");
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("priorVersion") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("priorVersion") != "")
+                priorVersion = xmlDoc.getElementsByTagName("object")[i].getAttribute("priorVersion");
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("deprecated") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("deprecated") != "")
+                deprecated = xmlDoc.getElementsByTagName("object")[i].getAttribute("deprecated");
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("incompatibleWith") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("incompatibleWith") != "")
+                incompatibleWith = xmlDoc.getElementsByTagName("object")[i].getAttribute("incompatibleWith");
+
+            if(xmlDoc.getElementsByTagName("object")[i].getAttribute("backwardCompatibleWith") != null || xmlDoc.getElementsByTagName("object")[i].getAttribute("backwardCompatibleWith") != "")
+                backwardCompatibleWith = xmlDoc.getElementsByTagName("object")[i].getAttribute("backwardCompatibleWith");
+
+            $('[id$=nome]').last().text(xmlDoc.getElementsByTagName("object")[i].getAttribute("label"));
+            $('[id$=SubClassOf]').last().text(SubClassOf);
+            $('[id$=DisjointWith]').last().text(DisjointWith);
+            $('[id$=Equivalence]').last().text(Equivalence);
+            $('[id$=TargetForKey]').last().text(TargetForKey);
+            $('[id$=Instances]').last().text(Instances);
+            $('[id$=label]').last().text(label);
+            $('[id$=seeAlso]').last().text(seeAlso);
+            $('[id$=isDefinedBy]').last().text(isDefinedBy);
+            $('[id$=comment]').last().text(comment);
+            $('[id$=versionInfo]').last().text(versionInfo);
+            $('[id$=priorVersion]').last().text(priorVersion);
+            $('[id$=deprecated]').last().text(deprecated);
+            $('[id$=incompatibleWith]').last().text(incompatibleWith);
+            $('[id$=backwardCompatibleWith]').last().text(backwardCompatibleWith);
+        }
+    }
 
 </script>
 @endpush
-    .
+    
 @stop
