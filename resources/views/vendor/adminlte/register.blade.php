a45d1c2a6094f8d35cc50e8a8fdc39bd992ed4d6
Lucas Piazzi <40416044+Piazzi@users.noreply.github.com>
2022-05-05 19:05:46 -0300

    Update register.blade.php

--- resources/views/vendor/adminlte/register.blade.php
+++ resources/views/vendor/adminlte/register.blade.php
@@ -96,9 +96,11 @@
                                 @endif
                             </div>
 
-                            <div class="form-group has-feedback {{ $errors->has('avatar_url') ? 'has-error' : '' }}">
-                                <input type="file" name="avatar_url" class="form-control">
-                            </div>
+                     
+                            
+                              <button style="background-color: #064c72;" type="submit"
+                                    class="btn btn-primary btn-block btn-flat"
+                            >{{ __('Register') }}</button>
 
                         </form>
                         <div class="auth-links">
